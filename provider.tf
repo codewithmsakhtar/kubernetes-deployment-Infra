bf2416847b68f40c8aee1fe359845c8f152cdf0d
Mohd Shahnawaz Akhtar <s.akhtar.ril@gmail.com>
2025-09-02 22:28:31 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ backend "azurerm" {
     resource_group_name  = "rg-githubaction"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "stggithubaction"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "container-githubaction"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "dev.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 
 }
